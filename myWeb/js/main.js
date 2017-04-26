6cd8f38c017716fb8fcdb3426bf9de7f36c9ba38
LelKikyou <724436883@qq.com>
2017-04-26 17:26:09 +0800

    zz

--- myWeb/js/main.js
+++ myWeb/js/main.js
@@ -30,9 +30,49 @@
     faceDown.onmouseenter = function () {
         this.style.width = "120px";
         this.style.height = "120px";
-    }
+    };
     faceDown.onmouseleave=function () {
         this.style.width = "100px";
         this.style.height = "100px";
+    }
+})();
+//基本资料
+(function () {
+    //封装查找所有兄弟节点的方法
+    function siblings(elm) {
+        var a = [];
+        var p = elm.parentNode.children;//可能children不兼容
+        for(var i =0,pl= p.length;i<pl;i++) {
+            if(p[i] !== elm) a.push(p[i]);
+        }
+        return a;
+    }
+
+    var myMainData=document.querySelectorAll(".myMainData>div");
+    var myMain=document.querySelector(".myMain");
+    for(var i=0;i<myMainData.length;i++){
+       +(function (i) {
+           myMainData[i].onmouseenter=function () {
+               //字消失
+               myMain.style.animation="zoomOutDown 0.5s forwards";
+               //移动上来
+               this.lastElementChild.children[0].style.top="330px";
+               //给其它的兄弟附上黑色
+               for (var n=0;n<siblings(this).length;n++){
+                   // 兄弟的子节点加上黑
+                   siblings(this)[n].lastElementChild.style.background="black";
+               }
+           };
+           myMainData[i].onmouseleave=function () {
+               //字出现
+               myMain.style.animation="zoomInDown 0.7s forwards";
+               //移动下去
+               this.lastElementChild.children[0].style.top="400px";
+               //给其它的兄弟去掉黑色
+               for (var n=0;n<siblings(this).length;n++){
+                   siblings(this)[n].lastElementChild.style.background=""
+               }
+           }
+       })(i)
     }
 })();
