8b2cd9e9ef40690ed84d879fe8b7444a5a3ec02f
LelKikyou <724436883@qq.com>
2017-05-05 18:23:32 +0800

    zz

--- myWeb/js/main.js
+++ myWeb/js/main.js
@@ -1,3 +1,11 @@
+// 获取当前的rem；
+(function () {
+    window.onresize=function () {
+        document.documentElement.style.fontSize=document.documentElement.clientWidth/10+"px";
+    };
+    document.documentElement.style.fontSize=document.documentElement.clientWidth/10+"px";
+})();
+
 //nav
 (function () {
     var nav = document.querySelector(".nav");
@@ -45,7 +53,6 @@ function siblings(elm) {
 }
 //基本资料
 (function () {
-
     var myMainData = document.querySelectorAll(".myMainData>div");
     var myMain = document.querySelector(".myMain");
     for (var i = 0; i < myMainData.length; i++) {
@@ -74,124 +81,124 @@ function siblings(elm) {
         })(i)
     }
 })();
-//项目经验
-(function () {
-    //展示项目部分
-    var proChange = document.querySelectorAll(".projectImg ul li");
-    var proImg = document.querySelectorAll(".proImg");
-    var proFilm = document.querySelectorAll(".proFilm");
-    var proSearch = document.querySelectorAll(".proFilm>div");
-    for (var i = 0; i < proChange.length; i++) {
-        +function (i) {
-            proChange[i].onmouseenter = function () {
-                proImg[i].style.left = "-30px";
-                proImg[i].style.top = "-30px";
-                proImg[i].style.width = "410px";
-                proImg[i].style.height = "410px";
-                //    改变黑色的膜
-                proFilm[i].style.left = "-30px";
-                proFilm[i].style.top = "-30px";
-                proFilm[i].style.width = "410px";
-                proFilm[i].style.height = "410px";
-                proFilm[i].style.background = "rgba(0,0,0,0.5)";
-                //    搜索跳出来
-                proSearch[i].style.animation = "bounceInDown 0.5s";
-                proSearch[i].style.animationFillMode = "forwards";
-            };
-            proChange[i].onmouseleave = function () {
-                proImg[i].style.left = 0;
-                proImg[i].style.top = 0;
-                proImg[i].style.width = "350px";
-                proImg[i].style.height = "350px";
-                //    改变黑色的膜
-                proFilm[i].style.left = 0;
-                proFilm[i].style.top = 0;
-                proFilm[i].style.width = "350px";
-                proFilm[i].style.height = "350px";
-                proFilm[i].style.background = "rgba(0,0,0,0)";
-                //    跳走
-                proSearch[i].style.animation = "bounceOutUp 0.5s";
-                proSearch[i].style.animationFillMode = "forwards";
-            }
-        }(i)
-    }
-    //展示导航部分
-    var projectNav = document.querySelectorAll(".projectNav li");
-    for (var n = 0; n < projectNav.length; n++) {
-        +function (n) {
-            projectNav[n].onclick = function () {
-                //nav颜色
-                this.classList.add("addColor");
-                for (var m = 0; m < siblings(this).length; m++) {
-                    siblings(this)[m].classList.remove("addColor");
-                }
-                //    展示部分的
-                if (n === 0) {
-                    proChange[0].style.display = "block";
-                    proChange[1].style.display = "block";
-                    proChange[2].style.display = "block"
-                } else if (n === 1) {
-                    proChange[0].style.display = "block";
-                    proChange[1].style.display = "block";
-                    proChange[2].style.display = "none"
-                } else if (n === 2) {
-                    proChange[0].style.display = "none";
-                    proChange[1].style.display = "none";
-                    proChange[2].style.display = "block"
-                }
-            }
-        }(n)
-    }
-})();
-//自我评价下的轮播
-(function () {
-    var myEva = document.querySelector("#myEva");
-    var myEvaShow = document.querySelector(".myEvaShow ul");
-    var myEvaBtn = document.querySelectorAll(".myEvaBtn li span");
-    var timer;
-    var timers;
-//    向右轮播
-    var pag = 0;
-
-    function rightCar() {
-        pag++;
-        myEvaShow.style.left = pag * -720 + "px";
-        if (pag > 3) {
-            pag = 0;
-            pag++;
-            myEvaShow.style.transition = "";
-            myEvaShow.style.left = 0;
-            timer = setTimeout(function () {
-                myEvaShow.style.transition = "all 0.5s linear";
-                myEvaShow.style.left = pag * -720 + "px";
-            }, 20)
-        }
-        btnMove()
-    }
-
-    timers = setInterval(rightCar, 3500);
-    myEva.onmouseenter = function () {
-        clearTimeout(timer);
-        clearInterval(timers);
-    };
-    myEva.onmouseleave = function () {
-        timers = setInterval(rightCar, 3500);
-    };
-    //按钮颜色跟着动
-    function btnMove() {
-        myEvaBtn[pag == 3 ? 0 : pag].classList.add("addColor");
-        for (var m = 0; m < siblings(myEvaBtn[pag == 3 ? 0 : pag].parentNode).length; m++) {
-            siblings(myEvaBtn[pag == 3 ? 0 : pag].parentNode)[m].firstElementChild.classList.remove("addColor");
-        }
-    }
-//    点击哪个按钮去哪。
-    for (var i = 0; i < myEvaBtn.length; i++) {
-        +function (i) {
-            myEvaBtn[i].onclick = function () {
-                pag = i;
-                myEvaShow.style.left = pag * -720 + "px";
-                btnMove();
-            };
-        }(i)
-    }
-})()
+// //项目经验
+// (function () {
+//     //展示项目部分
+//     var proChange = document.querySelectorAll(".projectImg ul li");
+//     var proImg = document.querySelectorAll(".proImg");
+//     var proFilm = document.querySelectorAll(".proFilm");
+//     var proSearch = document.querySelectorAll(".proFilm>div");
+//     for (var i = 0; i < proChange.length; i++) {
+//         +function (i) {
+//             proChange[i].onmouseenter = function () {
+//                 proImg[i].style.left = "-30px";
+//                 proImg[i].style.top = "-30px";
+//                 proImg[i].style.width = "410px";
+//                 proImg[i].style.height = "410px";
+//                 //    改变黑色的膜
+//                 proFilm[i].style.left = "-30px";
+//                 proFilm[i].style.top = "-30px";
+//                 proFilm[i].style.width = "410px";
+//                 proFilm[i].style.height = "410px";
+//                 proFilm[i].style.background = "rgba(0,0,0,0.5)";
+//                 //    搜索跳出来
+//                 proSearch[i].style.animation = "bounceInDown 0.5s";
+//                 proSearch[i].style.animationFillMode = "forwards";
+//             };
+//             proChange[i].onmouseleave = function () {
+//                 proImg[i].style.left = 0;
+//                 proImg[i].style.top = 0;
+//                 proImg[i].style.width = "350px";
+//                 proImg[i].style.height = "350px";
+//                 //    改变黑色的膜
+//                 proFilm[i].style.left = 0;
+//                 proFilm[i].style.top = 0;
+//                 proFilm[i].style.width = "350px";
+//                 proFilm[i].style.height = "350px";
+//                 proFilm[i].style.background = "rgba(0,0,0,0)";
+//                 //    跳走
+//                 proSearch[i].style.animation = "bounceOutUp 0.5s";
+//                 proSearch[i].style.animationFillMode = "forwards";
+//             }
+//         }(i)
+//     }
+//     //展示导航部分
+//     var projectNav = document.querySelectorAll(".projectNav li");
+//     for (var n = 0; n < projectNav.length; n++) {
+//         +function (n) {
+//             projectNav[n].onclick = function () {
+//                 //nav颜色
+//                 this.classList.add("addColor");
+//                 for (var m = 0; m < siblings(this).length; m++) {
+//                     siblings(this)[m].classList.remove("addColor");
+//                 }
+//                 //    展示部分的
+//                 if (n === 0) {
+//                     proChange[0].style.display = "block";
+//                     proChange[1].style.display = "block";
+//                     proChange[2].style.display = "block"
+//                 } else if (n === 1) {
+//                     proChange[0].style.display = "block";
+//                     proChange[1].style.display = "block";
+//                     proChange[2].style.display = "none"
+//                 } else if (n === 2) {
+//                     proChange[0].style.display = "none";
+//                     proChange[1].style.display = "none";
+//                     proChange[2].style.display = "block"
+//                 }
+//             }
+//         }(n)
+//     }
+// })();
+// //自我评价下的轮播
+// (function () {
+//     var myEva = document.querySelector("#myEva");
+//     var myEvaShow = document.querySelector(".myEvaShow ul");
+//     var myEvaBtn = document.querySelectorAll(".myEvaBtn li span");
+//     var timer;
+//     var timers;
+// //    向右轮播
+//     var pag = 0;
+//
+//     function rightCar() {
+//         pag++;
+//         myEvaShow.style.left = pag * -720 + "px";
+//         if (pag > 3) {
+//             pag = 0;
+//             pag++;
+//             myEvaShow.style.transition = "";
+//             myEvaShow.style.left = 0;
+//             timer = setTimeout(function () {
+//                 myEvaShow.style.transition = "all 0.5s linear";
+//                 myEvaShow.style.left = pag * -720 + "px";
+//             }, 20)
+//         }
+//         btnMove()
+//     }
+//
+//     timers = setInterval(rightCar, 3500);
+//     myEva.onmouseenter = function () {
+//         clearTimeout(timer);
+//         clearInterval(timers);
+//     };
+//     myEva.onmouseleave = function () {
+//         timers = setInterval(rightCar, 3500);
+//     };
+//     //按钮颜色跟着动
+//     function btnMove() {
+//         myEvaBtn[pag == 3 ? 0 : pag].classList.add("addColor");
+//         for (var m = 0; m < siblings(myEvaBtn[pag == 3 ? 0 : pag].parentNode).length; m++) {
+//             siblings(myEvaBtn[pag == 3 ? 0 : pag].parentNode)[m].firstElementChild.classList.remove("addColor");
+//         }
+//     }
+// //    点击哪个按钮去哪。
+//     for (var i = 0; i < myEvaBtn.length; i++) {
+//         +function (i) {
+//             myEvaBtn[i].onclick = function () {
+//                 pag = i;
+//                 myEvaShow.style.left = pag * -720 + "px";
+//                 btnMove();
+//             };
+//         }(i)
+//     }
+// })();
