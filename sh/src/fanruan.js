18a15ceb2bade35700c941c18a2d0a86adea779b
lelkikyou <724436883@qq.com>
2019-08-15 15:52:55 +0800

    z

--- sh/src/fanruan.js
+++
@@ -1,4 +0,0 @@
-const BASE_URL = 'http://183.194.248.142:8080/webroot/'
-export default {
-    BASE_URL
-}
--- 上海数据平台/src/api/URL.js
+++
@@ -1,9 +0,0 @@
-export const URL="http://localhost:11000/api/v1/dev";
-// export const URL="http://192.168.0.117:11000/api/v1/dev";
-
-//存cookie里面命名
-export const TOKEN='sdcmToken';
-//存Sessionstorage里面tab命名
-export const TAB='sdcmTab';
-//存Sessionstorage里面userinfo命名
-export const USERINFO='sdcmUserinfo';
